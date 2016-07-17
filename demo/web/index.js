e2460a87b36bf6a11e2c41cacfc1b52e137ebe43
qiongtubao <810776886@qq.com>
2016-07-17 23:58:33 +0800

    debug  reload require error

--- demo/web/index.js
+++ demo/web/index.js
@@ -1,10 +1,13 @@
 var config = {
 	port: 10065,
 	web: {
-		loadPath: "./web"
-	}
+		loadPath: "./web",
+		reloadTime: 1000
+	},
+	cpus:1
 };
 var Server = require("../../");
 var server = new Server(config);
+
 process.latte = server;
 server.run();
